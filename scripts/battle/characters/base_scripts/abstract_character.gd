76de3193dcacb768db21871a993f14203dab2256
LAPTOP-01MR4FIS\halim <halimovadalera@mail.ru>
2022-11-08 21:46:59 +0500

    ver. 0.0.3

---
+++ scripts/battle/characters/base_scripts/abstract_character.gd
@@ -0,0 +1,142 @@
+class_name AbstractCharacter
+extends Node2D
+
+
+enum Resistance {
+	IMMUNITY = 0, 
+	INEFFECTIVE = 50, 
+	WEAK = 75,
+	NORMAL = 100, 
+	HIGH = 150, 
+	FATAL = 200,
+}
+
+@export var name_character: String = ''
+
+@export_group("Health")
+@export_range(1, 1000, 1) var max_physical_health: int = 1
+@export_range(1, 1000, 1) var max_mental_health: int = 1
+
+@export_group("Resistances")
+@export var physical_resistance: Resistance = Resistance.NORMAL
+@export var mental_resistance: Resistance = Resistance.NORMAL
+
+@export_group("Speed Dice")
+@export_range(1, 99, 1) var min_speed: int = 1
+@export_range(1, 99, 1) var max_speed: int = 1
+@export_range(1, 10, 1) var speed_dice_count: int = 1
+
+var is_themself_placement_cards: bool = false
+
+@onready var is_ally: bool = self in get_tree().get_nodes_in_group("allies")
+
+@onready var physical_health: PhysicalHealth = PhysicalHealth.new(max_physical_health)
+@onready var mental_health: MentalHealth = MentalHealth.new(max_mental_health)
+
+@onready var character_marker_3d: CharacterMarker3D = preload(
+		"res://scenes/battle/characters/base_scenes/character_marker_3d.tscn").instantiate()
+
+@onready var subcharacter_bar: SubcharacterBars = $SubcharacterBars
+@onready var actions_animations: AnimatedSprite2D = $Actions
+@onready var speed_dice_manager: SpeedDiceManager = $SpeedDiceManager
+
+
+
+func _ready() -> void:
+	@warning_ignore(return_value_discarded)
+	physical_health.died.connect(_on_died)
+	@warning_ignore(return_value_discarded)
+	mental_health.stunned.connect(_on_stunned)
+	subcharacter_bar.init(physical_health, mental_health)
+	
+	speed_dice_manager.set_speed(min_speed, max_speed)
+	speed_dice_manager.change_speed_dice_count(speed_dice_count, is_ally)
+
+
+func _process(_delta: float) -> void:
+	position = character_marker_3d.get_current_position_on_camera()
+
+
+func prepare_for_card_placement() -> void:
+	character_marker_3d.return_to_starting_position()
+	flip_to_starting_position()
+	roll_speed_dice()
+
+
+func prepare_for_combat() -> void:
+	pass
+
+
+func roll_speed_dice() -> void:
+	speed_dice_manager.roll_speed_dice()
+
+
+func take_damage(damage: int) -> void:
+	take_physical_damage(damage)
+	take_mantal_damage(damage)
+
+
+func take_physical_damage(damage: int) -> void:
+	var physical_damage: int = roundi(damage * physical_resistance / 100.0)
+	physical_health.decrease(physical_damage)
+
+
+func take_mantal_damage(damage: int) -> void:
+	var mental_damage: int = roundi(damage * mental_resistance / 100.0)
+	mental_health.decrease(mental_damage)
+
+
+func die() -> void:
+	take_physical_damage(max_physical_health)
+
+
+func stun() -> void:
+	take_mantal_damage(max_mental_health)
+
+
+func place_cards_themself() -> Dictionary:
+	return {}
+
+
+func flip_to_starting_position() -> void:
+	var window_width: int = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var start_position: Vector2 = character_marker_3d.get_start_position_on_camera()
+	actions_animations.flip_h = start_position.x < window_width / 2.0
+
+
+func flip_to_specified_point(point_position: Vector2) -> void:
+	actions_animations.flip_h = position < point_position
+
+
+func flip_view_direction() -> void:
+	actions_animations.flip_h = !actions_animations.flip_h
+
+
+func actions_switcher(action: BattleParameters.Action) -> void:
+	match action:
+		BattleParameters.Action.DEFAULT:
+			actions_animations.play("default")
+		BattleParameters.Action.STUN:
+			actions_animations.play("stun")
+		BattleParameters.Action.MOVEMENT:
+			actions_animations.play("movement")
+		BattleParameters.Action.BLOCK:
+			actions_animations.play("block")
+		BattleParameters.Action.EVADE:
+			actions_animations.play("evade")
+		BattleParameters.Action.SLASH_ATTACK:
+			actions_animations.play("slash_attack")
+		BattleParameters.Action.PIERCE_ATTACK:
+			actions_animations.play("pierce_attack")
+		BattleParameters.Action.BLUNT_ATTACK:
+			actions_animations.play("blunt_attack")
+		_:
+			actions_animations.play("default")
+
+
+func _on_died() -> void:
+	pass
+
+
+func _on_stunned() -> void:
+	pass
---
+++ scripts/ui/battle_ui/characters_ui/speed_dice/speed_dice_manager/speed_dice_manager.gd
@@ -0,0 +1,61 @@
+class_name SpeedDiceManager
+extends Control
+
+
+var min_speed: int = 1
+var max_speed: int = 10
+
+var speed_dice_count: int = 0 :
+	get: return _speed_dice_container.get_child_count()
+
+@onready var _speed_dice_container: HBoxContainer = $HBoxContainer
+
+
+func set_speed(min_value: int, max_value: int) -> void:
+	min_speed = min_value
+	max_speed = max_value
+
+
+func change_speed_dice_count(new_count: int, is_ally: bool) -> void:
+	if new_count == speed_dice_count and new_count <= 0:
+		return
+	
+	var difference: int =  new_count - speed_dice_count
+	if difference > 0:
+		add_speed_dice(difference, is_ally)
+	else:
+		remove_speed_dice(-difference)
+
+
+func add_speed_dice(count: int, is_ally: bool) -> void:
+	for i in count:
+		var speed_dice: AbstractSpeedDice = (
+			preload("res://scenes/ui/battle_ui/characters_ui/speed_dice/speed_dice/ally_speed_dice.tscn").instantiate()
+			if is_ally 
+			else preload("res://scenes/ui/battle_ui/characters_ui/speed_dice/speed_dice/enemy_speed_dice.tscn").instantiate()
+		)
+		_speed_dice_container.add_child(speed_dice)
+
+
+func remove_speed_dice(count: int) -> void:
+	for i in count:
+		var speed_dice: AbstractSpeedDice = _speed_dice_container.get_child(0)
+		_speed_dice_container.remove_child(speed_dice)
+
+
+func roll_speed_dice() -> void:
+	var speed_dice_values: Array[int] = _get_sorted_speed()
+	for i in speed_dice_count:
+		var speed_dice: AbstractSpeedDice = _speed_dice_container.get_child(i)
+		var speed: int = speed_dice_values[i]
+		speed_dice.set_speed(speed)
+
+
+func _get_sorted_speed() -> Array[int]:
+	var speeds: Array[int] = []
+	for i in speed_dice_count:
+		var speed: int = GlobalParameters.random.randi_range(min_speed, max_speed)
+		speeds.append(speed)
+	
+	speeds.sort_custom(func(x, y): return x > y)
+	return speeds
